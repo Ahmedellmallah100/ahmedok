2691797367a1b264162349198ac09bffc78d69ff
Ahmed Elmallah <ah2005510@gmail.com>
2026-09-13 20:32:17 +0300

    Refactor clk_divider for reset and clock toggling
    Refactor clk_divider module to use negedge for reset and initialize clk_hz.

--- src/clk_divider.v
+++ src/clk_divider.v
@@ -1,17 +1,25 @@
-module clk_divider (clk ,rstn ,clk_hz);
-input clk , rstn;
-output reg clk_hz;
-reg [17:0] count;
+module clk_divider (
+    input  wire clk,
+    input  wire rstn,
+    output reg  clk_hz
+);
 
-always @(posedge clk or posedge rstn) begin
-    if (rstn) begin
-        count <= 18'd0;
+    reg [17:0] count;
+
+    always @(posedge clk or negedge rstn) begin
+        if (!rstn) begin
+            count <= 18'd0;
+            clk_hz <= 1'b0;
+        end
+        else begin
+            if (count == 18'd250000) begin
+                count <= 18'd0;
+                clk_hz <= ~clk_hz;
+            end
+            else begin
+                count <= count + 18'd1;
+            end
+        end
     end
-    else if (count < 250000) begin
-        count <= count+1;
-    end
-    else
-        clk_hz <= ~clk_hz;
-        count <= 17'd0;
-end
-endmodule //clk_divider
+
+endmodule
