37524e45292826bfd6b27af787ef914214ffc37d
Ahmed Elmallah <ah2005510@gmail.com>
2026-09-14 22:56:11 +0300

    Update Bcd_Conventer.v

--- src/Bcd_Conventer.v
+++ src/Bcd_Conventer.v
@@ -8,10 +8,10 @@ wire [3:0] hundred;
 wire [3:0] tens;
 wire [3:0] ones;
 
-assign hundred = binary / 4'd100;
-assign temp    = binary % 4'd100;
-assign tens    = temp / 4'd10;
-assign ones    = temp % 4'd10;
+assign hundred = binary / 100;
+assign temp    = binary % 100;
+assign tens    = temp / 10;
+assign ones    = temp % 10;
 
 assign bcd = {hundred, tens, ones};
 
