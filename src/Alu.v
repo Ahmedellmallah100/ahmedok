4c14665ac4ad5e8e204a0df7efb3f986304b8acf
Ahmed Elmallah <ah2005510@gmail.com>
2026-09-15 23:07:50 +0300

    Update Alu.v

--- src/Alu.v
+++ src/Alu.v
@@ -1,52 +1,62 @@
 module Alu (
-    input        rstn, clk, pb1_db, pb2_db, pb3_db,
+    input        rstn,
+    input        clk,
+    input        pb1_db,
+    input        pb2_db,
+    input        pb3_db,
     input  [7:0] sw,
-    output reg [7:0] c_plus_carry
-
+    output reg [8:0] c_plus_carry
 );
 
 reg [7:0] a, b;
 
+always @(posedge clk or negedge rstn) begin
 
-    always @(posedge clk or negedge rstn) begin
-
-        if (!rstn) begin
+    if (!rstn) begin
         a            <= 8'd0;
         b            <= 8'd0;
         c_plus_carry <= 9'd0;
-        letters      <= 4'd0;
-        opcode       <= 3'd0;
+
     end else begin
+
         if (pb1_db) begin
-            a       <= sw;
-            letters <= 4'hA;
+            a <= sw;
+
         end else if (pb2_db) begin
-            b       <= sw;
-            letters <= 4'hB;
+            b <= sw;
+
         end else if (pb3_db) begin
-               
+
             case (sw[2:0])
-                3'b001 : begin
+
+                3'b001: begin
                     c_plus_carry <= {1'b0, a} + {1'b0, b};
                 end
-                3'b010 : begin
+
+                3'b010: begin
                     c_plus_carry <= {1'b0, a} - {1'b0, b};
                 end
-                3'b011 : begin
+
+                3'b011: begin
                     c_plus_carry <= {1'b0, (~a + 8'd1)};
                 end
-                3'b100 : begin
+
+                3'b100: begin
                     c_plus_carry <= {1'b0, (a & b)};
                 end
-                3'b101 : begin
+
+                3'b101: begin
                     c_plus_carry <= {1'b0, (a | b)};
                 end
-                3'b110 : begin
+
+                3'b110: begin
                     c_plus_carry <= {1'b0, (a ^ b)};
                 end
+
                 default: begin
                     c_plus_carry <= 9'd0;
                 end
+
             endcase
         end
     end
