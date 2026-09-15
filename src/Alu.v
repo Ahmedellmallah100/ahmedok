24a4e31324a6a3faa45b13297af8e5e5da4b62ba
Ahmed Elmallah <ah2005510@gmail.com>
2026-09-15 21:39:32 +0300

    Update Alu.v

--- src/Alu.v
+++ src/Alu.v
@@ -2,7 +2,7 @@ module Alu (
     input        rstn, clk, pb1_db, pb2_db, pb3_db,
     input  [7:0] sw,
     output reg [7:0] c_plus_carry,
-    output reg [3:0] letters
+
 );
 
 reg [7:0] a, b;
@@ -28,31 +28,24 @@ reg [7:0] a, b;
             case (sw[2:0])
                 3'b001 : begin
                     c_plus_carry <= {1'b0, a} + {1'b0, b};
-                    letters <= 4'h1; 
                 end
                 3'b010 : begin
                     c_plus_carry <= {1'b0, a} - {1'b0, b};
-                    letters <= 4'h2; 
                 end
                 3'b011 : begin
                     c_plus_carry <= {1'b0, (~a + 8'd1)};
-                    letters <= 4'h3; 
                 end
                 3'b100 : begin
                     c_plus_carry <= {1'b0, (a & b)};
-                    letters <= 4'h4; 
                 end
                 3'b101 : begin
                     c_plus_carry <= {1'b0, (a | b)};
-                    letters <= 4'h5; 
                 end
                 3'b110 : begin
                     c_plus_carry <= {1'b0, (a ^ b)};
-                    letters <= 4'h6; 
                 end
                 default: begin
                     c_plus_carry <= 9'd0;
-                    letters <= 4'h0;
                 end
             endcase
         end
