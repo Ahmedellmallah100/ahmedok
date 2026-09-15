532b852cc450094e151db7f76bbcb5c848d417a1
Ahmed Elmallah <ah2005510@gmail.com>
2026-09-15 20:57:40 +0300

    Update decoder.v

--- src/decoder.v
+++ src/decoder.v
@@ -5,17 +5,24 @@ module decoder (digtial ,seg);
 
   always @ (*) begin 
     case (digtial)
-      4'd0 : seg = 7'b0000001; // 0
-      4'd1 : seg = 7'b1001111; // 1
-      4'd2 : seg = 7'b0010010; // 2
-      4'd3 : seg = 7'b0000110; // 3
-      4'd4 : seg = 7'b1001100; // 4
-      4'd5 : seg = 7'b0100100; // 5
-      4'd6 : seg = 7'b0100000; // 6
-      4'd7 : seg = 7'b0001111; // 7
-      4'd8 : seg = 7'b0000000; // 8
-      4'd9 : seg = 7'b0000100; // 9
-      default : seg = 7'b1111111;
+        case (digital)
+            4'h0: seg = 7'b0000001;
+            4'h1: seg = 7'b1001111;
+            4'h2: seg = 7'b0010010;
+            4'h3: seg = 7'b0000110;
+            4'h4: seg = 7'b1001100;
+            4'h5: seg = 7'b0100100;
+            4'h6: seg = 7'b0100000;
+            4'h7: seg = 7'b0001111;
+            4'h8: seg = 7'b0000000;
+            4'h9: seg = 7'b0000100;
+            4'hA: seg = 7'b0001000; // A
+            4'hB: seg = 7'b1100000; // b
+            4'hC: seg = 7'b0110001; // C
+            4'hD: seg = 7'b1000010; // d
+            4'hE: seg = 7'b0110000; // E
+            4'hF: seg = 7'b0111000; // F
+            default: seg = 7'b1111111;
     endcase
   end
 
